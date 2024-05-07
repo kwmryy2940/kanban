cdd9338cfc361a26dff244d0c37ea8ba0368abed
kwmryy2940 <mailbox.kawamura@gmail.com>
2024-05-07 10:26:33 +0900

    メイン画面作成、ドラッグ機能実装

---
+++ vue-frontend/src/components/Dashboard.vue
@@ -0,0 +1,127 @@
+<template>
+  <div>
+    <v-container>
+    <h1>Dashboard</h1>
+      <v-row>
+        <v-col class="col border ma-1">
+          <h4>ToDos</h4>
+          <draggable
+            v-model="tasks.todos"
+            group="my-group"
+            item-key="id"
+            class="dragArea"
+            animation="200"
+            handle=".handle"
+          >
+            <template #item="{ element }">
+              <v-list>
+                <v-card class="handle">
+                  <v-list-item>
+                    {{ element.name }}
+                  </v-list-item>
+                </v-card>
+              </v-list>
+            </template>
+          </draggable>
+        </v-col>
+        <v-col class="col border ma-1">
+          <h4>InProgress</h4>
+          <draggable
+            v-model="tasks.inProgress"
+            group="my-group"
+            item-key="id"
+            class="dragArea"
+            animation="200"
+            handle=".handle"
+          >
+            <template #item="{ element }">
+              <v-list>
+                <v-card class="handle">
+                  <v-list-item>
+                    {{ element.name }}
+                  </v-list-item>
+                </v-card>
+              </v-list>
+            </template>
+          </draggable>
+        </v-col>
+         <v-col class="col border ma-1">
+          <h4>Complete</h4>
+          <draggable
+            v-model="tasks.completed"
+            group="my-group"
+            item-key="id"
+            class="dragArea"
+            animation="200"
+            handle=".handle"
+          >
+            <template #item="{ element }">
+              <v-list>
+                <v-card class="handle">
+                  <v-list-item>
+                    {{ element.name }}
+                  </v-list-item>
+                </v-card>
+              </v-list>
+            </template>
+          </draggable>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script setup>
+import { ref, watch } from "vue";
+import draggable from "vuedraggable";
+
+const tasks = ref({
+  todos: [
+    {
+      id: 1,
+      name: "にんじん",
+    },
+    {
+      id: 2,
+      name: "じゃがいも",
+    },
+    {
+      id: 3,
+      name: "たまねぎ",
+    },
+    {
+      id: 4,
+      name: "カレールー",
+    },
+    {
+      id: 5,
+      name: "鶏肉",
+    },
+  ],
+  inProgress: [],
+  completed: [],
+});
+
+
+watch(tasks.todos, (val) => {
+  console.log("todos:", val);
+});
+watch(tasks, (inProgress) => {
+  console.log("inProgress:", val);
+});
+
+</script>
+
+<style scoped>
+.col {
+  height: 90vh;
+  overflow: auto;
+}
+.dragArea {
+  padding: 10px;
+  background-color: aqua;
+}
+.handle {
+  cursor: pointer;
+}
+</style>
--- vue-frontend/src/main.js
+++ vue-frontend/src/main.js
@@ -1,5 +1,20 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import router from "./router";
 
-createApp(App).mount('#app')
+// Vuetify のインポートを追加 **********/
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+const app = createApp(App);
+app.use(vuetify);
+app.use(router);
+app.mount("#app");
