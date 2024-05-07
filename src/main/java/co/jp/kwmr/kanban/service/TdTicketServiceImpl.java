fbf84d4f39b6332ebc63360779463c9371173f55
kwmryy2940 <mailbox.kawamura@gmail.com>
2024-05-07 14:20:19 +0900

    チケットデータ更新、削除用API作成

--- src/main/java/co/jp/kwmr/kanban/service/TdTicketServiceImpl.java
+++ src/main/java/co/jp/kwmr/kanban/service/TdTicketServiceImpl.java
@@ -2,6 +2,7 @@ package co.jp.kwmr.kanban.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.data.domain.Sort;
 
 import co.jp.kwmr.kanban.entity.TdTicketEntity;
+import co.jp.kwmr.kanban.entity.TmUsersEntity;
 import co.jp.kwmr.kanban.repository.TdTicketRepository;
 
 @Service
@@ -31,8 +33,8 @@ public class TdTicketServiceImpl implements TdTicketService {
 
   // @Override
   // public List<TdTicketEntity> getJoinTickets() {
-  //   List<TdTicketEntity> tdTicketList = tdTicketRepository.findJoinAll();
-  //   return tdTicketList;
+  // List<TdTicketEntity> tdTicketList = tdTicketRepository.findJoinAll();
+  // return tdTicketList;
   // }
 
   @Override
@@ -52,4 +54,29 @@ public class TdTicketServiceImpl implements TdTicketService {
     tdTicketRepository.save(ticket);
   }
 
+  @Override
+  public void updateTicketById(TdTicketEntity ticket, Integer ticketId) {
+    Optional<TdTicketEntity> ticket1 = tdTicketRepository.findById(ticketId);
+    TdTicketEntity tmp = ticket1.get();
+
+    // ユーザーからの入力データをセット
+    tmp.setUserId(ticket.getUserId());
+    tmp.setCategoryId(ticket.getCategoryId());
+    tmp.setStatus(ticket.getStatus());
+    tmp.setTicketTitle(ticket.getTicketTitle());
+    tmp.setTicketDetail(ticket.getTicketDetail());
+
+    // システムから現在時刻を取得
+    LocalDateTime now = LocalDateTime.now();
+    // 更新日時に現在時刻をセット
+    tmp.setUpdatedAt(now);
+
+    // DBに登録
+    tdTicketRepository.save(tmp);
+  }
+
+  @Override
+  public void deleteTicketById(Integer ticketId) {
+    tdTicketRepository.deleteById(ticketId);
+  }
 }
